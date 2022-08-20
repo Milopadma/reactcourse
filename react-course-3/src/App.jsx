7419866649272bb49cb336991269b97d5ffc127f
milo <48256865+Milopadma@users.noreply.github.com>
2022-08-20 22:43:39 +0800

    commit #2
    pass term data from child component of SearchBar up to App using callbacks

--- react-course-3/src/App.jsx
+++ react-course-3/src/App.jsx
@@ -1,13 +1,20 @@
 import SearchBar from "./components/SearchBar";
 import ImageList from "./components/ImageList";
+import React from "react";
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="ui container">
-      <SearchBar />
-    </div>
-  );
+  onSearchSubmit = (term) => { //receive term from child component
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-course-3/src/components/SearchBar.jsx
+++ react-course-3/src/components/SearchBar.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    state = { term: '' }; //hold the value term of the input field as a state so that it doesnt need to be fetched from DOM everytime
+  state = { term: "" }; //hold the value term of the input field as a state so that it doesnt need to be fetched from DOM everytime
+  
+  handleInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault(); //prevent the default behaviour of the form submit event
+    this.props.onSubmit(this.state.term); //pass the term to the parent component
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })} //"inform" the state what the current input value is
+              onChange={this.handleInputChange} //"inform" the state what the current input value is
             />
           </div>
         </form>
