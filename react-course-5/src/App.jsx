8861e14aa7885686ffd4be738886d74683d5074a
milo <48256865+Milopadma@users.noreply.github.com>
2022-08-25 15:35:45 +0800

    working accordion using useState

--- react-course-5/src/App.jsx
+++ react-course-5/src/App.jsx
@@ -3,24 +3,23 @@ import Accordion from "./components/Accordion";
 const items = [
   {
     title: "What is React?",
-    content: "React is a front end javascript framework",
+    content: "React is a front end javascript framework.",
   },
   {
     title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
+    content: "React is a favorite JS library among engineers.",
   },
   {
     title: "How do you use React?",
-    content: "You use React by creating components",
+    content: "You use React by creating components.",
   },
 ];
 
 function App() {
   return (
     <div className="App">
-      fuck mark
-      <br/>
-      <Accordion items={items}/>
+      <br />
+      <Accordion items={items} />
     </div>
   );
 }
--- react-course-5/src/components/Accordion.jsx
+++ react-course-5/src/components/Accordion.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Accordion({ items }) {
-  const renderedItems = items.map((items) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const onTitleClick = (index) => {
+    setActiveIndex(index); 
+  };
+
+  const renderedItems = items.map((items, index) => {
+    const active = index === activeIndex ? "active" : "";
     // render each item in the array
     return (
       <React.Fragment key={items.title}>
-        <div className="title active">
+        <div
+          className={`title ${active}`}
+          onClick={() => {
+            onTitleClick(index);
+          }}
+        >
           <i className="dropdown icon"> </i>
           {items.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{items.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>; // return the rendered items
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+    </div>
+  ); // return the rendered items
 }
+
+
