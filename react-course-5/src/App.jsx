e06a6d2d4a48e11d28087afb3f099f7b3ab2436a
milo <48256865+Milopadma@users.noreply.github.com>
2022-08-25 22:43:39 +0800

    dropdown component

--- react-course-5/src/App.jsx
+++ react-course-5/src/App.jsx
@@ -1,5 +1,6 @@
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -16,6 +17,20 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 function App() {
   return (
     <div className="App">
@@ -23,8 +38,10 @@ function App() {
       <br />
       {/* <h3>Accordion</h3> */}
       {/* <Accordion items={items} /> */}
-      <h3>Wikipedia Search</h3>
-      <Search />
+      {/* <h3>Wikipedia Search</h3>
+      <Search /> */}
+      <h3>Dropdown</h3>
+      <Dropdown options={options} />
     </div>
   );
 }
