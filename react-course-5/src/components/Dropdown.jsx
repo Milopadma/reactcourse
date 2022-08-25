e37132d3f16978e1de7f9454a08d71f0bc760a31
milo <48256865+Milopadma@users.noreply.github.com>
2022-08-25 22:52:43 +0800

    dropdown structure

--- react-course-5/src/components/Dropdown.jsx
+++ react-course-5/src/components/Dropdown.jsx
@@ -1,7 +1,27 @@
-export default function Dropdown() {
+import { useState } from "react";
+
+export default function Dropdown({ options }) {
+  const [Selection, setSelection] = useState(null);
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.label}>
+        <button>{option.label}</button>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h1>Dropdown</h1>
+      <div className="ui selection dropdown">
+        <div className="field"></div>
+        <label className="label">Select Option</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select Option</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
     </div>
   );
 }
