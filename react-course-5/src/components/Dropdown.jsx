1ea1703362771e7b988e7b4b12442b3d59e7201b
milo <48256865+Milopadma@users.noreply.github.com>
2022-08-26 00:07:25 +0800

    click outside dropdown closes it

--- react-course-5/src/components/Dropdown.jsx
+++ react-course-5/src/components/Dropdown.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Dropdown({ options, selected, onSelectedChange }) {
   const [Open, setOpen] = useState(false);
+  const ref = useRef();
 
   const renderedOptions = options.map((option) => {
     //dont show currently selected option in the dropdown
@@ -11,15 +12,33 @@ export default function Dropdown({ options, selected, onSelectedChange }) {
     //show the options in the dropdown
     return (
       <div key={option.label}>
-        <button onClick={() => onSelectedChange(option)}>{option.label}</button>
+        <button className="item" onClick={() => onSelectedChange(option)}>
+          {option.label}
+        </button>
       </div>
     );
   });
 
+  useEffect(() => {
+    document.body.addEventListener(
+      //listen to clicks outside the dropdown
+      "click",
+      () => {
+        if (ref.current.contains(event.target)) {
+          //if the click is inside the dropdown, do nothing
+          return;
+        }
+        //if the click is outside the dropdown
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
+
   return (
     <div>
       <h1>Dropdown</h1>
-      <div className="ui selection dropdown">
+      <div ref={ref} className="ui form">
         <div className="field"></div>
         <label className="label">Select Option</label>
         <div
