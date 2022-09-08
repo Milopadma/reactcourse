8cd438dd76d6a69b2d45f40823ba7eda055998fd
milo <48256865+Milopadma@users.noreply.github.com>
2022-09-08 15:24:35 +0800

    react routing scaffold

--- react-course-8/client/src/App.jsx
+++ react-course-8/client/src/App.jsx
@@ -1,11 +1,27 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
+import StreamCreate from "./components/streams/StreamCreate";
+import StreamEdit from "./components/streams/StreamEdit";
+import StreamDelete from "./components/streams/StreamDelete";
+import StreamList from "./components/streams/StreamList";
+import StreamShow from "./components/streams/StreamShow";
+
+//components
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
