23ed34a80d33c3a4bf627b4ce5438f871e97287a
milo <48256865+Milopadma@users.noreply.github.com>
2022-09-14 15:30:01 +0800

    implemented streaming video using OBS and node media server

--- react-course-8/client/src/components/streams/StreamShow.jsx
+++ react-course-8/client/src/components/streams/StreamShow.jsx
@@ -2,9 +2,38 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
+import FlvJs from "flv.js";
+
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  // helper method to build the player
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = FlvJs.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -13,6 +42,12 @@ class StreamShow extends React.Component {
     }
     return (
       <div>
+        <video
+          ref={this.videoRef}
+          src=""
+          style={{ width: "100%" }}
+          controls
+        ></video>
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
