77515edadd0c26f7a313c0011684d32029a13058
milo <48256865+Milopadma@users.noreply.github.com>
2022-09-15 15:33:00 +0800

    init new react course project; context system material

---
+++ react-course-9/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import UserCreate from "../components/UserCreate";
+
+class App extends React.Component {
+  state = { language: "english" };
+
+  // callback function for the onClick event
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          <h1>Select a language:</h1>
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
