ff608a4852651281bb913b012491166cc6001e2f
milo <48256865+Milopadma@users.noreply.github.com>
2022-09-15 16:13:25 +0800

    using multiple contexts

--- react-course-9/src/App.jsx
+++ react-course-9/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import UserCreate from "./components/UserCreate";
 import LanguageContext from "./contexts/LanguageContext";
+import ColorContext from "./contexts/ColorContext";
 
 class App extends React.Component {
-  state = { language: "english" };
+  state = { language: "english", color: "primary" };
 
   // callback function for the onClick event
-  onLanguageChange = (language) => {
-    this.setState({ language });
+  onLanguageChange = (language, color) => {
+    this.setState({ language, color });
   };
 
   render() {
@@ -17,17 +18,20 @@ class App extends React.Component {
           <h1>Select a language:</h1>
           <i
             className="flag uk"
-            onClick={() => this.onLanguageChange("english")}
+            onClick={() => this.onLanguageChange("english", "primary")}
           />
           <i
             className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
+            onClick={() => this.onLanguageChange("dutch", "red")}
           />
         </div>
-        {/* Provider component is used to provide 
+        {/* Provider components are used to provide 
         a value to all child components*/}
+        {/* // using multiple providers */}
         <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
+          <ColorContext.Provider value={this.state.color}>
+            <UserCreate />
+          </ColorContext.Provider>
         </LanguageContext.Provider>
       </div>
     );
--- react-course-9/src/components/Button.jsx
+++ react-course-9/src/components/Button.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   render() {
     // Consumer component is used to consume the
     // value provided by the Provider component.
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {(value) => (value === "english" ? "Submit" : "Voorleggen")}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {/* Consumers need to get passed a function as its child*/}
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
     );
   }
 }
